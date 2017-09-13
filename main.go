540ffe5438e4225274d332a3d85a7c5d4f1eafeb
timothy1999 <tim950196@hotmail.com>
2017-09-13 16:55:52 +0800

    Update main.go

--- main.go
+++ main.go
@@ -54,7 +54,14 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 					log.Print(err)
 				
 				}
-				startedd = true^startedd
+				if startedd{
+					startedd=false
+				
+				}else{
+					startedd=true
+				
+				}
+				
  				}
 			case *linebot.ImageMessage:
 				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.ID+":"+"抽"+" OK!")).Do(); err != nil {
